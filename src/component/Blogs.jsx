8d24c2d334d8a46355fd742524dd35f69d853ce5
wptasmina <wptasmina@gmail.com>
2025-04-24 12:03:13 +0600

    added dark text color update and text size update

--- src/component/Blogs.jsx
+++ src/component/Blogs.jsx
@@ -2,6 +2,7 @@
 import { FaRegHeart } from 'react-icons/fa'
 import { TbListDetails } from 'react-icons/tb'
 import { NavLink, useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 export default function Blogs({ blog }) {
   const { userName, email, userImage, title, imageUrl, date, shortDesc, longDesc, category} = blog
@@ -30,7 +31,7 @@ return (
 <> 
 <div data-aos="fade-up"
      data-aos-duration="3000" 
-     className="card bg-base-100 dark:bg-gray-900 p-4 border shadow-xl">
+     className="card bg-base-200 dark:bg-gray-900 p-4 border dark:border-gray-800 shadow-xl">
   {/* user name and image  */}
   <div className='flex flex-row items-center pb-2 gap-2'>
    <img src={userImage} className='w-12 h-12 object-cover rounded-full shadow border border-purple-600 p-1' alt="author image" />
@@ -48,31 +49,30 @@ return (
      alt="Product image"
    />
  </figure>
-
-  
- <div className=" space-y-4">
-   <h2 className="card-title text-2xl text-gray-800 font-bold pt-4">{title}</h2>
+ 
+ <div className=" space-y-4 dark:bg-gray-900">
+   <h2 className="card-title text-2xl text-gray-800 dark:text-gray-300 font-bold pt-4">{title}</h2>
    <div className='flex justify-between items-center'>
      <div>
        <p className='text-md text-indigo-800 font-bold'>{category}</p>
      </div>
     <div>
-      <p className='font-medium text-gray-900'>{date}</p>
+      <p className='font-medium text-gray-900 dark:text-gray-400'>{date}</p>
     </div>
    </div>
-   <p className='font-normal'>{shortDesc}...see Moer</p>
+   <p className='font-normal'>{shortDesc}<span className=' text-green-600'><Link to={`/details/${blog._id}`}>...see Moer</Link></span></p>
    {/* <p className='text-gray-600 text-md font-normal'>{longDesc}</p> */}
 
    <div className="card-actions flex justify-center md:gap-2 gap-4 pt-4">
     <NavLink to={`/details/${blog._id}`}>
-     <button className="btn bg-white text-white/70 text-white bg-gradient-to-r from-indigo-800 via-purple-600 to-[#2b2a2a] px-4">
-       <TbListDetails />
+     <button className="inline-flex items-center bg-white font-medium bg-gradient-to-r from-indigo-800 via-purple-600 to-[#2b2a2a] bg-clip-text text-transparent border border-purple-600 text-[#74256e] px-4 py-2 rounded-md">
+       <TbListDetails className=" mr-1" />
        Details
      </button>
     </NavLink>
 
-    <button onClick={hendleWishList} className="btn bg-white text-white/70 bg-gradient-to-r from-indigo-800 via-purple-600 to-[#2b2a2a] px-4">
-      <FaRegHeart />
+    <button onClick={hendleWishList} className="inline-flex items-center bg-white font-medium bg-gradient-to-r from-indigo-800 via-purple-600 to-[#2b2a2a] bg-clip-text text-transparent border border-purple-600 text-[#74256e] px-4 py-2 rounded-md">
+      <FaRegHeart className=" mr-1" />
       Wishlist
     </button>
    </div>
